5e6afcd5e0f2d957227fc775ed53fe4a82d660eb
Jamal <jamalrahimzadegan@gmail.com>
2022-01-21 23:52:23 +0330

    Update regex.ts

--- src/regex.ts
+++ src/regex.ts
@@ -1,4 +1,4 @@
-export const sharedRegex = {
+export default {
   IS_MOBILE_DEVICE: /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/,
   IS_MOBILE_NUM: /^09[\d]{9}$/,
   PER_NUM: /[٠١٢٣٤٥٦٧٨٩]/g,
